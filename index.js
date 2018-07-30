8e16dc2ad3e9fe9c6faf2a4e39a9f70644be647a
HakuryuuDom <abohn49@yahoo.com>
2018-07-30 02:58:56 -0400

    meh, didnt work

--- index.js
+++ index.js
@@ -23,31 +23,13 @@ module.exports = function Drop(mod) {
 	});
 
 	mod.command.add('drop', percent => {
-		if(percent === null) {
-			if(mod.settings.defaultPercent === null) {
-				mod.command.message('Please input a percent to drop. Your desired choice will be saved for future use.')
-				return;
-			}
-			percent = mod.settings.defaultPercent //load from config
-		}
 		percent = Number(percent);
 
-		if(!(percent > 0 && percent <= 100) || !curHp) {
-			mod.command.message('Error: ' + percent + '% is not between 1% and 100%, or Current HP is unknown.')
-			return;
-		}
-		mod.settings.defaultPercent = percent //save to config
-		if(mod.settings.firstUse) {
-			mod.command.message('New Update! Your percent to drop has been saved. You may now use /8 drop (without a number) to drop your HP to the saved percent.')
-			mod.settings.firstUse = false;
-		}
+		if(!(percent > 0 && percent <= 100) || !curHp) return;
+
 		let percentToDrop = (curHp * 100 / maxHp) - percent;
 
-		if(percentToDrop <= 0) {
-			mod.command.message('Error: Your HP cannot be zero or negative!')
-			return;
-		}
-		
+		if(percentToDrop <= 0) return;
 
 		mod.send('C_PLAYER_LOCATION', 5, Object.assign({}, location, {
 			loc: location.loc.addN({z: 400 + percentToDrop * (mod.game.me.race === 'castanic' ? 20 : 10)}),
